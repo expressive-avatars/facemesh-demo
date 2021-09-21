3fe5a120db41e7e8e05c259f86b81cf5a62b8d04
mattrossman <rossman@gatech.edu>
2021-09-21 19:59:29 -0400

    Send ImageData to web worker

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react"
+import Worker from "./worker?worker"
+
+/** @type {(url: string) => Promise<HTMLImageElement>} */
+function loadImage(src) {
+  return new Promise((resolve) => {
+    const img = new Image()
+    img.addEventListener("load", () => resolve(img))
+    img.crossOrigin = "anonymous"
+    img.src = src
+  })
+}
+
+const canvas = document.createElement("canvas")
+const ctx = canvas.getContext("2d")
+/** @type {(img: HTMLImageElement) => ImageData} */
+function getImageData(img) {
+  canvas.width = img.width
+  canvas.height = img.height
+  ctx.drawImage(img, 0, 0)
+  return ctx.getImageData(0, 0, img.width, img.height)
+}
+
+export default function App() {
+  useEffect(() => {
+    loadImage("/obama.png").then((img) => {
+      const worker = new Worker()
+      const imageData = getImageData(img)
+      worker.postMessage(imageData, [imageData.data.buffer])
+    })
+  }, [])
+  return <p>Hello</p>
+}
